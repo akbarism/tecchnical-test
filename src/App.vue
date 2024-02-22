022c1c909c0bf35a52530e82cbfb17c50e67f5aa
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2024-02-22 08:33:50 +0700

    Setup root style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,87 @@
 <script setup></script>
 
 <template>
-  <h1>Header</h1>
-  <router-view></router-view>
+  <main class="main">
+    <aside class="aside pl-8 pr-5">
+      <h1 class="logo mt-6 mb-5">heylink.</h1>
+      <ul>
+        <li class="mt-3">
+          <router-link
+            to="/"
+            class="link list-none p-3 text-black rounded-md flex items-center"
+            ><img
+              src="/images/hara_dashboard.svg"
+              alt="icon overview"
+              class="mr-3 block"
+            />Dashboard</router-link
+          >
+        </li>
+        <li class="mt-3">
+          <router-link
+            to="/overview"
+            class="link list-none p-3 text-black rounded-md flex items-center"
+          >
+            <img
+              src="/images/hara_overview.svg"
+              alt="icon overview"
+              class="mr-3 block"
+            />
+            <h1>Overview</h1></router-link
+          >
+        </li>
+      </ul>
+    </aside>
+    <section class="content">
+      <div class="head text-black p-3 pl-6">Conversion</div>
+      <div class="main">
+        <router-view></router-view>
+      </div>
+    </section>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main {
+  position: relative;
+}
+
+.aside {
+  background: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 281px;
+}
+
+.aside .logo {
+  font-family: Inter;
+  font-size: 2em;
+  font-weight: 800;
+  line-height: 36px;
+  letter-spacing: 0em;
+  text-align: left;
+}
+
+.aside ul li .link {
+  background-color: #e3f2fd;
+}
+
+.content {
+  margin-left: 281px;
+}
+
+.content .head {
+  background: white;
+  font-family: Inter;
+  font-size: 1em;
+  font-weight: 600;
+  line-height: 21px;
+  letter-spacing: 0em;
+  text-align: left;
+}
+
+.content .main {
+  margin-left: 37px;
+}
+</style>
